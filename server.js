1129f51c721bc30365d483e2d341d235383ba186
Nestor Fock <njfock@nuestrodiario.com.gt>
2020-05-03 00:01:23 -0600

    add router

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function (req, res) {
-    res.send('Hola');
+app.use(router);
+
+router.get('/message', function (req, res){
+    res.send('Lista de mensajes');
 });
+router.delete('/message', function (req, res){
+    res.send('Borrar mensaje');
+});
+//app.use('/', function (req, res) {
+//    res.send('Hola');
+//});
 
 app.listen(3000);
 console.log('La app escucha en http://localhost:3000');
