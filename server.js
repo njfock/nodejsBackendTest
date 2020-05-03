4efef5e35d8108f9fb2445efc2e095654300c0f5
Nestor Fock <njfock@nuestrodiario.com.gt>
2020-05-03 00:18:13 -0600

    add body-parser

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
 router.get('/message', function (req, res){
     res.send('Lista de mensajes');
 });
-router.delete('/message', function (req, res){
-    res.send('Borrar mensaje');
+
+router.put('/message', function (req, res){
+    console.log(req.query)
+    console.log(req.body)
+    res.send('Mensaje '+req.body.text+' añadido correctamente');
 });
-//app.use('/', function (req, res) {
-//    res.send('Hola');
-//});
 
 app.listen(3000);
 console.log('La app escucha en http://localhost:3000');
