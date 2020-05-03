3046590a8b1acd36d3b0c5c2a30919d134d57944
Nestor Fock <njfock@nuestrodiario.com.gt>
2020-05-03 13:37:36 -0600

    add console.error

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ router.get('/message', function (req, res){
 router.post('/message', function (req, res){
     //console.log(req.query)
     if(req.query.error == 'ok'){
-        response.error(req, res, 'Error simulado');
+        response.error(req, res, 'Error inesperado', 500, 'Es solo una simulación de los errores');
     } else {
         response.success(req, res, 'Mensaje '+req.body.text+' añadido correctamente', 201);
     }
